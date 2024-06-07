6dade905fd9546ce1a758d78a503165c970da73c
warrenwalt <warrenwalt19@gmail.com>
2024-06-07 19:39:36 +0300

    change pricing

--- constants/index.ts
+++ constants/index.ts
@@ -71,11 +71,11 @@ export const plans = [
     _id: 2,
     name: "Pro Package",
     icon: "/assets/icons/free-plan.svg",
-    price: 40,
-    credits: 120,
+    price: 4,
+    credits: 100,
     inclusions: [
       {
-        label: "120 Credits",
+        label: "100 Credits",
         isIncluded: true,
       },
       {
@@ -96,11 +96,11 @@ export const plans = [
     _id: 3,
     name: "Premium Package",
     icon: "/assets/icons/free-plan.svg",
-    price: 199,
-    credits: 2000,
+    price: 79,
+    credits: 1000,
     inclusions: [
       {
-        label: "2000 Credits",
+        label: "1000 Credits",
         isIncluded: true,
       },
       {
